8cb7882f460d75d3102da271476c7030713ea108
Suagrcothe <ifeanyivalentine82@gmail.com>
2022-10-17 14:47:42 +0100

    dsa

--- primenumber.js
+++ primenumber.js
@@ -17,3 +17,20 @@ function Prime(n) {
 console.log(Prime(1));
 console.log(Prime(5));
 console.log(Prime(4));
+
+// But if we want to reduce the time complexity, we can use sqrt to reduce the time the code runs
+function Prime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(Prime(1));
+console.log(Prime(5));
+console.log(Prime(4));
